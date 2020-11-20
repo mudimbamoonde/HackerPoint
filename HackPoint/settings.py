9529e3f16d6780481aabd9d36d52f544dc372e5d
Mudimba Moonde <mudimbamoonde@gmail.com>
2020-11-20 16:59:29 +0200

    Db

--- HackPoint/settings.py
+++ HackPoint/settings.py
@@ -74,10 +74,20 @@ WSGI_APPLICATION = 'HackPoint.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME':'billedcall',
+        'USER':'root',
+        'PASSWORD':'',
+        'HOST':'localhost',
     }
 }
 
